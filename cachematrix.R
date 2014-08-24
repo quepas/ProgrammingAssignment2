2ee641c49dd10d7a551011c6363acddadad9b76a
Quepas <oneinchman.dev@gmail.com>
2014-08-25 01:02:22 +0200

    Adding cacheable invere matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+###################################################################
+## Below functions give ability to invert the matrix into the cache.
+## It is an important task because inversion of matrix
+## is computationally expensive.
+##
+## @author: Patryk Kiepas (quepas)
+###################################################################
 
-## Write a short comment describing this function
+## Function creates cacheable matrix object
+makeCacheMatrix <- function(base_matrix = matrix()) {
+  inverted_matrix <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+  setMatrix <- function(new_matrix) {
+    base_matrix <<- new_matrix
+    inverted_matrix <<- NULL
+  }
+  getMatrix <- function() {
+    base_matrix
+  }
+  setInvMatrix <- function(new_matrix) {
+    inverted_matrix <<- new_matrix
+  }
+  getInvMatrix <- function() {
+    inverted_matrix
+  }
 
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
 }
 
+## Function computes inverse of the cacheable matrix (only if needed)
+cacheSolve <- function(cached_matrix, ...) {
+  ## Return a matrix that is the inverse of 'cached_matrix'
+  inverted_matrix <- cached_matrix$getInvMatrix()
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## If inverse matrix already calculated - return it!
+  if (!is.null(inverted_matrix)) {
+    message("Getting inverse matrix from cache")
+    return (inverted_matrix)
+  }
+  ## Otherwise, compute inverse matrix
+  matrix <- cached_matrix$getMatrix()
+  inverted_matrix <- solve(matrix, ...)
+  cached_matrix$setInvMatrix(inverted_matrix)
+  inverted_matrix
 }
